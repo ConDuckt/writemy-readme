f56545a5b68c34b1271966458675c9922324deff
AMiro <aj.miro.b@gmail.com>
2023-03-28 17:46:11 -0500

    Add GitHub username input/link

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ const questions = [
         name: "license",
         message: "Enter license information for your project:",
     },
+    {
+        type: "input",
+        name: "github-username",
+        message: "Enter your GitHub username:",
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -76,9 +81,6 @@ ${data["usage"]}
 
 ${data["contributing"]}
 
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
 ## Tests
 
 ${data["tests"]}
@@ -89,7 +91,9 @@ ${data["license"]}
 
 ## Questions
 
-${data["questions"]}
+[Click here](https://github.com/${data["github-username"]}) to contact me on GitHub. 
+
+Alternatively, you may reach me at ${data["questions"]}.
 
     `;
     
@@ -113,17 +117,6 @@ function init() {
 init();
 
 // TODO:
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
 // WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// 
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
